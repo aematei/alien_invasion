fd248c85b5c797832ebe0bfa26dc3126dcb77774
Alex Matei <dirtyburb11@gmail.com>
2018-02-28 17:24:49 -0800

    Updated to give ship left/right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,11 +13,12 @@ def run_game():
 	pygame.display.set_caption('Alien Invasion')
 
 	# Make a ship.
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
 
-	# Start main loop fpr the game.
+	# Start main loop for the game.
 	while True:
-		gf.check_events()
+		gf.check_events(ship)
+		ship.update()
 		gf.update_screen(ai_settings, screen, ship)
 
 run_game()
